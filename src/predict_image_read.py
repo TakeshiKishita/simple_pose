cb7332f8029725d6ec7e10a622f36908059ef36c
tkc <xxtkcxx@gmail.com>
2019-07-19 01:57:18 +0900

    リアルタイムなカメラ映像の取得

--- src/predict_image_read.py
+++ src/predict_image_read.py
@@ -1,85 +1,81 @@
-import sys
 import cv2
 from logging import getLogger
-
 from multiprocessing import Process, Queue
 
-class CameraReader:
+
+class VideoReader:
     def __init__(self, src):
         self.logger = getLogger(__name__)
         self.src = src
+
+        # 幅, 高さ, フレーム数, フレームレートの初期化
+        self.width, self.height, self.frame_num, self.fps = None, None, None, None
+        self.queue = None
+        self.process = None
 
     def open(self):
         """
         openCVインスタンスの初期化
         """
-        self.cap = cv2.VideoCapture(self.src)
-
         # マルチプロセスで、フレームを格納するキューを作成
         self.queue = Queue(maxsize=1)
         # マルチプロセスで、フレームを取得し続ける
-        self.p = Process(target=get_frame, args=(self.cap, self.queue))
-        self.p.start()
+        self.process = Process(target=self.get_frame, args=(self.src, self.queue))
+        self.process.start()
 
     def read(self):
         """
         カメラから取得した映像に対して、推論を行う
         """
         while True:
-            try:
-                # キューを取得（この処理後にキューが空になる）
-                bgr_img = self.queue.get()
+            # キューからイメージを取得（この処理後にキューが空になる）
+            bgr_img = self.queue.get()
 
-                return bgr_img
+            yield bgr_img
 
-            # キーボードの「Ctrl-C」が押された場合、終了処理を行う
-            except (KeyboardInterrupt):
-                self.logger.warning('pushed Ctrl-C')
-                self.close()
-    
     def close(self):
         """
-        処理を終了する
+        処理を終了
         """
-        self.cap.release()
+        self.process.terminate()
         self.queue.close()
-        self.p.terminate()
-    
+
     def __dell__(self):
-        self.cap.release()
-        self.queue.close()
-        self.p.terminate()
+        self.close()
 
+    @staticmethod
+    def get_frame(src, queue):
+        """
+        フレームを取得
+        キューが空の場合、フレームをキューに入れ続ける
+        Args:
+            src: opencvで読み込むカメラソース（バイス番号、URL）
+            queue:キュー
+        """
+        cap = cv2.VideoCapture(src)
 
-def get_frame(cap, queue):
-    """
-    フレームを取得し、キューが空の場合、
-    フレームをキューに入れ続ける。
-    Args:
-        cap:VideoCaptureオブジェクト
-        queue:キュー
-    """
-    while True:
-        # フレームを取得する
-        _, frame = cap.read()
+        try:
+            while True:
+                # フレームを取得する
+                _, frame = cap.read()
 
-        # キューが空の場合、キューにフレームを入れる。
-        if queue.empty():
-            queue.put(frame)
+                # キューが空の場合、キューにフレームを入れる。
+                if queue.empty():
+                    queue.put(frame)
+        finally:
+            cap.release()
 
 
 if __name__ == '__main__':
-    src = 'http://210.254.207.10:80/-wvhttp-01-/GetOneShot?image_size=640x480&frame_count=1000000000'
-    c = CameraReader(src)
+    source = 0
+    c = VideoReader(source)
     c.open()
     try:
-        while True:
-            img = c.read()
+        for img in c.read():
             cv2.imshow('test', img)
-            cv2.waitKey(0)
-    except:
+            cv2.waitKey(1)
+    except KeyboardInterrupt:
+        print('pushed Ctrl-C')
+    finally:
         c.close()
-        print('ERROR')
-    c.close()
     cv2.destroyAllWindows()
-
